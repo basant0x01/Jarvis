9537c94f25166112916f19ecc4378fee3aa41aaf
Basant Karki <123530150+basant0x01@users.noreply.github.com>
2024-04-20 19:39:04 +0545

    Update view-total-subdomains.php

--- lazydb-0.2-beta/admin/view-total-subdomains.php
+++ lazydb-0.2-beta/admin/view-total-subdomains.php
@@ -1,14 +1,14 @@
 <?php
 include "db.php";
 
-$query = "SELECT program_subdomains FROM my_projects";
+$query = "SELECT program_subdomains, program_manual_subdomains FROM my_projects";
 $result = mysqli_query($con, $query);
 
 $subdomains = '';
 
 if ($result) {
     while ($row = mysqli_fetch_assoc($result)) {
-        $subdomain_string = $row['program_subdomains'];
+        $subdomain_string = $row['program_subdomains'] . ',' . $row['program_manual_subdomains'];
         $subdomain_array = explode(",", $subdomain_string); // Assuming subdomains are separated by commas
         foreach ($subdomain_array as $subdomain) {
             // Trim to remove leading/trailing whitespaces
